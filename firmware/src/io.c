9ebcd7dd3f2cc7f11b26f3f6bbe16523e0874a2a
João Antônio Cardoso <joao.maker@gmail.com>
2019-12-07 23:47:05 -0300

    exploring a difference approach for IO interfaces

---
+++ firmware/src/io.c
@@ -0,0 +1,118 @@
+#include "io.h"
+#include "conf.h"
+
+void leds_init(void)
+{
+#ifdef IO_ON
+    leds_t config = {
+        .all = LEDS_DDR,
+        .led0 = direction_output,
+        .led1 = direction_output,
+        .led2 = direction_output,
+        .led3 = direction_output,
+    };
+    
+    LEDS_DDR = config.all;
+
+    leds.led0 = led_on;
+    leds.led1 = led_on;
+    leds.led2 = led_on;
+    leds.led3 = led_on;
+    
+    _delay_ms(100);
+
+    leds.led0 = led_off;
+    leds.led1 = led_off;
+    leds.led2 = led_off;
+    leds.led3 = led_off;
+#endif
+}
+
+void switches_init(void)
+{
+#ifdef IO_ON
+    switches_direction(direction_input);
+#endif
+}
+
+void switches_read(void)
+{
+#ifdef IO_ON
+    switches.all = SW_PIN;
+#endif 
+}
+
+void switches_direction(direction_t direction)
+{
+#ifdef IO_ON
+    switches_t config = {
+        .all = SW_DDR,
+        .sw0 = direction,
+        .sw1 = direction,
+        .sw2 = direction,
+        .sw3 = direction,
+        .sw4 = direction,
+        .sw5 = direction,
+        .sw6 = direction,
+        .sw7 = direction,
+        .sw8 = direction,
+        .sw9 = direction,
+    };
+
+    DDRC = config.high;
+    DDRD = config.low;
+
+    if(direction == direction_input){
+        config = (switches_t){
+            .all = SW_PORT,
+            .sw0 = pullup_enabled,
+            .sw1 = pullup_enabled,
+            .sw2 = pullup_enabled,
+            .sw3 = pullup_enabled,
+            .sw4 = pullup_enabled,
+            .sw5 = pullup_enabled,
+            .sw6 = pullup_enabled,
+            .sw7 = pullup_enabled,
+            .sw8 = pullup_enabled,
+            .sw9 = pullup_enabled,
+        };
+
+        PORTC = config.high;
+        PORTD = config.low;
+    }
+#endif 
+}
+
+void buzzer_beep(uint16_t ms)
+{
+#ifdef IO_ON
+    buzzer.buzzer0 = buzzer_on;
+    _delay_ms(ms);
+    buzzer.buzzer0 = buzzer_off;
+    _delay_ms(ms);
+#endif 
+}
+
+void buzzer_init(void)
+{
+#ifdef IO_ON
+    buzzer_t config = {
+        .all = BUZZER_DDR,
+        .buzzer0 = direction_output,
+    };
+    BUZZER_DDR = config.all;
+
+    buzzer_beep(buzzer_beep1);
+    _delay_ms(buzzer_beep4);
+
+    buzzer_beep(buzzer_beep1);
+    _delay_ms(buzzer_beep4);
+
+    buzzer_beep(buzzer_beep1);
+    _delay_ms(buzzer_beep4);
+    
+    buzzer_beep(buzzer_beep1);
+    _delay_ms(buzzer_beep4);
+#endif 
+}
+
---
+++ firmware/src/io.h
@@ -0,0 +1,103 @@
+/**
+ * @file io.h
+ *
+ * @defgroup IO IO Module
+ *
+ * @brief Specific module for buzzer, leds and switches interfaces.
+ *
+ */
+
+#ifndef IO_H
+#define IO_H
+           
+#include <avr/io.h>
+#include <stdlib.h>
+#include "../lib/bit_utils.h"
+#include "conf.h"
+#include <util/delay.h>
+#include "dbg_vrb.h"
+#include "usart.h"
+
+typedef enum { direction_input, direction_output } direction_t;
+typedef enum { pullup_disabled, pullup_enabled } pullup_t;
+
+typedef union{
+    struct{
+        uint16_t sw4    :1,  // PC0
+                 sw8    :1,  // PC1
+                 sw1    :1,  // PC2
+                 sw6    :1,  // PC3
+                        :1,  // PC4
+                        :1,  // PC5
+                        :1,  // PC6
+                        :1,  // PC7
+                        :1,  // PD0
+                        :1,  // PD1
+                 sw3    :1,  // PD2
+                 sw2    :1,  // PD3
+                 sw7    :1,  // PD4
+                 sw9    :1,  // PD5
+                 sw0    :1,  // PD6
+                 sw5    :1;  // PD7
+    };
+    struct{
+        uint16_t high   :8,  // C
+                 low    :8;  // D
+    };
+    uint16_t all;
+} switches_t;
+
+#define SW_PIN 	(((PIND) << 8) | (PINC))
+#define SW_PORT	(((PORTD) << 8) | (PORTC))
+#define SW_DDR 	(((DDRD) << 8) | (DDRC))
+
+switches_t switches;
+
+typedef union{
+    struct{
+        uint8_t led0    :1,  // PE0
+                led1    :1,  // PE1
+                led2    :1,  // PE2
+                led3    :1,  // PE3
+                        :4;  // others
+    };
+    uint8_t all;
+} leds_t;
+
+typedef enum { led_off, led_on } led_state_t;
+
+#define leds    	(*(volatile leds_t *)&PORTE)
+#define LEDS_DDR 	(DDRE)
+
+typedef union{
+    struct{
+        uint8_t buzzer0 :1,   // PB0
+                        :7;   // others
+    };
+    uint8_t all;
+} buzzer_t;
+
+typedef enum { buzzer_on, buzzer_off } buzzer_state_t;
+typedef enum { 
+    buzzer_beep0 = 5,
+    buzzer_beep1 = 10,
+    buzzer_beep2 = 25,
+    buzzer_beep3 = 50,
+    buzzer_beep4 = 100,
+    buzzer_beep5 = 200,
+    buzzer_beep6 = 500,
+} buzzer_beep_t;
+
+#define buzzer    	(*(volatile buzzer_t *)&PORTB)
+#define BUZZER_DDR  (DDRB)
+
+void leds_init(void);
+void switches_init(void);
+void buzzer_init(void);
+
+void switches_read(void);
+void switches_direction(direction_t direction);
+
+void buzzer_beep(uint16_t ms);
+
+#endif
